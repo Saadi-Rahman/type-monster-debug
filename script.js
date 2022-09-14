47b02db408fd309227b8d4aaa2d7f89120d8708b
Saadi-Rahman <sadi6280@gmail.com>
2022-09-15 03:18:58 +0600

    Countdown has been hidden successfully

--- script.js
+++ script.js
@@ -117,6 +117,7 @@ const start = () => {
       display.classList.remove("inactive");
 
       clearInterval(startCountdown);
+      countdownOverlay.style.display = "";
       startTime = new Date().getTime();
     }
     count--;
